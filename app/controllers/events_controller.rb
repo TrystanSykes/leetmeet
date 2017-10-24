585e0f9a79f85cb7dab69f1a7b3aec5ab6f8c016
Trystan Sykes <trystansykes@gmail.com>
2017-10-24 15:14:51 +1100

    create header and configured contollers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,16 +1,60 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
 
   def show
+    @event = Event.find(params[:id])
   end
 
   def new
+
+  end
+
+  def create
+    event = Event.new
+    event.user_id = params[:user_id]
+    event.title = params[:title]
+    event.location = params[:location]
+    event.game_id = params[:game_id]
+    event.category_id = params[:category_id]
+    event.about = params[:about]
+    event.active = true
+    if event.save
+      redirect_to "/events/#{event.id}"
+    else
+      @errors = event.errors.full_messages
+      render :new
+    end
   end
 
   def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    event = Event.find(params[:id])
+    event.user_id = params[:user_id]
+    event.title = params[:title]
+    event.location = params[:location]
+    event.game_id = params[:game_id]
+    event.category_id = params[:category_id]
+    event.about = params[:about]
+    if event.save
+      redirect_to "/events/#{event.id}"
+    else
+      @errors = event.errors.full_messages
+      render :edit
+
   end
 
   def delete
+    @event = Event.find(params[:id]
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+    redirect_to '/events'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,49 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def new
+
+  end
+
+  def create
+    user = User.new
+    user.username = params[:username]
+    user.email = params[:email]
+    user.password = params[:password]
+    user.avatar_url = params[:avatar_url]
+    user.bio = params[:bio]
+    if user.save
+      redirect_to "/users/#{user.id}"
+    else
+      @errors = user.errors.full_messages
+      render :new
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.username = params[:username]
+    user.avatar_url = params[:avatar_url]
+    user.bio = params[:bio]
+    if user.save
+      redirect_to "/users/#{user.id}"
+    else
+      @errors = user.errors.full_messages
+      render :edit
   end
 
   def delete
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to '/'
   end
 end
