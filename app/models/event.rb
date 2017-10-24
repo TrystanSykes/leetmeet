c31e4426de1eda33694e2d97e93735d0c74ff0b4
Trystan Sykes <trystansykes@gmail.com>
2017-10-25 09:04:45 +1100

    added active record validations and fixed bugs

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,9 @@ class Event < ApplicationRecord
   belongs_to :game
   belongs_to :category
   has_many :attendees
+
+  validates :title, presence: true
+  validates :location, presence: true
+  validates :about, presence: true, length: { minimum: 40 }
+
 end
