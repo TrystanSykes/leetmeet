b0f462f827b62895ec054c47e8ea879dc86d05f7
Fiona Jessica <fionajessica23@gmail.com>
2017-10-25 13:54:18 +1100

    create api folder for making json

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
-get '/events/delete/:id', to: 'events#delete'
+  get '/events/delete/:id', to: 'events#delete'
   resources :events
-get '/users/delete/:id', to: 'users#delete'
+  get '/users/delete/:id', to: 'users#delete'
   resources :users
 
-get '/sessions/new', to: 'sessions#new'
-post '/sessions', to: 'sessions#create'
-delete '/sessions/:id', to: 'sessions#destroy'
+  get '/sessions/new', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+  delete '/sessions/:id', to: 'sessions#destroy'
+
+  get '/api/events', to: 'api/events#index'
 
   root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
