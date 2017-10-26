0eb0a78923bf016cbfbaf7bc710f0097e7af56d9
Fiona Jessica <fionajessica23@gmail.com>
2017-10-26 11:57:16 +1100

    anything

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   has_many :game_interests
   has_many :games, through: :game_interests
 
-  # validates :username, uniqueness: true, presence: true
-  # validates :email, uniqueness: true, presence: true
-  # validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  # validates :password, presence: true, length: { minimum: 8 }
-  # validates :avatar_url, presence: true
+  validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :avatar_url, presence: true
 
 end
