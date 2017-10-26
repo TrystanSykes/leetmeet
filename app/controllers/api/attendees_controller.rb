9141cdc2fb2b336624477ef75c7cd09a9abc5f1f
Lyle Innes <lyle.innes@gmail.com>
2017-10-26 15:58:06 +1100

    moved edit delete buttons. fixed attendee list

--- app/controllers/api/attendees_controller.rb
+++ app/controllers/api/attendees_controller.rb
@@ -4,8 +4,9 @@ class Api::AttendeesController < ApplicationController
     attendee = Attendee.new
     attendee.user_id = params[:user_id]
     attendee.event_id = params[:event_id]
+    username = attendee.user.username
     if attendee.save
-      render json: attendee
+      render json: {username: username, user_id: attendee.user_id}
     else
       render json: attendee.errors
     end
