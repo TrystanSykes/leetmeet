e74772707407926cb93ece2d908b27eb106c0031
Jon Kang <Jon.Kang.JD@gmail.com>
2017-10-26 21:27:08 +1100

    added datetime to new and edit events, altered events db - added 2 columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025055324) do
+ActiveRecord::Schema.define(version: 20171026062908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20171025055324) do
     t.datetime "updated_at", null: false
     t.float "lat"
     t.float "long"
+    t.datetime "start_at"
+    t.datetime "end_at"
     t.index ["category_id"], name: "index_events_on_category_id"
     t.index ["game_id"], name: "index_events_on_game_id"
     t.index ["user_id"], name: "index_events_on_user_id"
