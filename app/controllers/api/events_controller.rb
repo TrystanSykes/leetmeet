dddfaf300a74ae6489eb14e838bef7075674ecca
Trystan Sykes <trystansykes@gmail.com>
2017-10-27 12:29:29 +1100

    made logos from db work

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -3,10 +3,10 @@ class Api::EventsController < ApplicationController
   def index
     if params[:id]
       @event = Event.find(params[:id])
-      render json: @event
+      render json: @event, :include => :game
     else
       @events = Event.all
-      render json: @events
+      render json: @events, :include => :game
     end
   end
 
