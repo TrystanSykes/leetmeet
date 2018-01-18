46415b8910124822a229be23d62c22c9abf5cc5b
Trystan Sykes <trystansykes@gmail.com>
2018-01-18 12:37:06 +1100

    changed to private ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :stage, :production
 
-server '52.62.226.213', user: 'ubuntu', roles: %w{app web db}
+server '172.31.9.74', user: 'ubuntu', roles: %w{app web db}
 
 # server-based syntax
 # ======================
